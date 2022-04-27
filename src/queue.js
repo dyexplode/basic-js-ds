14dafe9349766fbac17012c904e0b68994f056db
Vladimir <admin@ttp.by>
2022-04-27 23:37:58 +0300

    feat: two excercise completed

--- src/queue.js
+++ src/queue.js
@@ -1,5 +1,5 @@
 const { NotImplementedError } = require('../extensions/index.js');
-
+const { ListNode } = require('../extensions/list-node.js');
 // const { ListNode } = require('../extensions/list-node.js');
 
 /**
@@ -13,20 +13,56 @@ const { NotImplementedError } = require('../extensions/index.js');
  * queue.dequeue(); // returns the top element from queue and deletes it, returns 1
  * queue.getUnderlyingList() // returns { value: 3, next: null }
  */
+class LinkedList{
+  constructor(){
+    this.head = null;
+    this.tail = null;
+  }
+
+  //add new item in LinkedList in end
+  append(value){
+    const newItem = new ListNode(value);
+    // if head or tail not yet
+    if (!this.head || !this.tail){
+      this.head = newItem;
+      this.tail = newItem
+    }
+
+    //if this inited
+    this.tail.next = newItem;
+    this.tail = newItem;
+  }
+
+  //Load from Array
+  createFromArray(arr){
+    arr.forEach(value => this.append(value));
+  }
+}
+
 class Queue {
+  constructor(){
+    this.qList = [];
+  }
 
   getUnderlyingList() {
-    throw new NotImplementedError('Not implemented');
+    const u = new LinkedList();
+    u.createFromArray(this.qList);
+    return u.head;
+    
+    
+    //throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
   }
 
-  enqueue(/* value */) {
-    throw new NotImplementedError('Not implemented');
+  enqueue(value) {
+    this.qList.push(value);
+    //throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
   }
 
   dequeue() {
-    throw new NotImplementedError('Not implemented');
+    return this.qList.shift();
+    //throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
   }
 }
