7bf5ad3a77711973ef5cfffcbb2e477ba2061d61
Vladimir <admin@ttp.by>
2022-04-27 14:26:22 +0300

    feat: Add decision stack

--- src/stack.js
+++ src/stack.js
@@ -13,18 +13,24 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 class Stack {
-  push(/* element */) {
-    throw new NotImplementedError('Not implemented');
+  constructor(){
+    this.estack = [];
+  }
+  push(element) {
+    this.estack.push(element);
+    //throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
   }
 
   pop() {
-    throw new NotImplementedError('Not implemented');
+    return this.estack.pop();
+    //throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
   }
 
   peek() {
-    throw new NotImplementedError('Not implemented');
+    return this.estack[this.estack.length - 1]
+    //throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
   }
 }
